5fdb1a494271f099a8f62cbb6f68ad03b8b408ed
Keaton <punmasterkeat@gmail.com>
2026-01-15 13:59:50 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,40 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
-# Capabilities & Setup
+# Capabilities
 appium = UiAutomator2Options()
 appium.platform_name = "Android"
-appium.device_name = "ANDROID NAME (TBD)"
+appium.device_name = "Pixel 4 XL"
 appium.automation_name = "UiAutomator2"
-appium.app = "C:/path/to/your/app.apk"  # CHANGE THIS to your APK path
+appium.app = None
+appium.app_package = "com.videotron.helixtv"
+appium.app_activity = "com.xfinity.common.view.LaunchActivity"
 appium.ensure_webviews_have_pages = True
 appium.native_instruments_lib = True
 
 # Connect to Appium Server
-driver = webdriver.Remote("http://127.0.0.1:4723", appium=appium)
+driver = webdriver.Remote("http://127.0.0.1:4723", options=appium)
 
 try:
-    print("Session Started Successfully!")
+    print("Starting Session...")
     
-    # Example: Finding an element by Accessibility ID (the best way)
-    login_button = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value='Login')
-    login_button.click()
+    # Take screenshot to see current state
+    driver.save_screenshot("screenshot.png")
+    print("Screenshot taken")
     
-    # Wait for 5 seconds so you can see it open
-    import time
+    # Wait up to 10 seconds for the Get Started button to appear
+    wait = WebDriverWait(driver, 10)
+    get_started_button = wait.until(
+        EC.presence_of_element_located((AppiumBy.ID, 'com.videotron.helixtv:id/get_started_button'))
+    )
+    print("Get Started button found!")
+    get_started_button.click()
+    
+    # Wait for 5 seconds
     time.sleep(5)
 
 finally:
