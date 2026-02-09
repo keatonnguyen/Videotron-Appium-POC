43d0e16845900477c4f99ab6163b19754fdea3ff
Keaton Nguyen <punmasterkeat@gmail.com>
2026-02-09 16:01:41 -0500

    fixes

--- helixTV2/pages/liveTv_page.py
+++ helixTV2/pages/liveTv_page.py
@@ -1,17 +1,17 @@
 from .base_page import BasePage
-from ..utils.locators import HomeLocators
+from ..utils.locators import LiveTVLocators
 
 class LiveTvPage(BasePage):
 
     def go_to_channel_4(self):
-        self.click(HomeLocators.CHANNEL_4)
+        self.click(LiveTVLocators.CHANNEL_4)
     
     def go_to_channel_340(self):
-        self.click(HomeLocators.CHANNEL_340)
+        self.click(LiveTVLocators.CHANNEL_340)
     
     def open_filter(self):
-        self.click(HomeLocators.FILTER_BUTTON)
+        self.click(LiveTVLocators.FILTER_BUTTON)
     
     def select_favorites_filter(self):
-        self.click(HomeLocators.FILTER_FAVORITES_OPTION)
-        self.click(HomeLocators.FILTER_CONFIRM_BUTTON)
+        self.click(LiveTVLocators.FILTER_FAVORITES_OPTION)
+        self.click(LiveTVLocators.FILTER_CONFIRM_BUTTON)
